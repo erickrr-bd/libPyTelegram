9df58ff3f12c27887685900bc66d13ef974426e9
Erick Rodriguez <erickrr.tbd93@gmail.com>
2022-04-13 10:47:55 -0500

    Add sendFileMessageTelegram method

--- libPyTelegram/libPyTelegram.py
+++ libPyTelegram/libPyTelegram.py
@@ -1,6 +1,6 @@
-from pycurl import Curl, HTTP_CODE
+from io import StringIO
 from urllib.parse import urlencode
-
+from pycurl import Curl, HTTP_CODE, FORM_FILE
 
 class libPyTelegram:
 
@@ -20,6 +20,28 @@ class libPyTelegram:
 		data = {'chat_id' : telegram_chat_id, 'text' : message_to_send}
 		pf = urlencode(data)
 		c.setopt(c.POSTFIELDS, pf)
+		c.perform_rs()
+		status_code = c.getinfo(HTTP_CODE)
+		c.close()
+		return status_code
+
+
+	def sendFileMessageTelegram(self, telegram_bot_token, telegram_chat_id, message_to_send, file_to_send):
+		"""
+		Method that sends a message and an attached file using the Telegram API.
+
+		:arg telegram_bot_token: Telegram bot token that will be used to send the messages.
+		:arg telegram_chat_id: Identifier of the Telegram channel where the messages will be sent.
+		:arg message_to_send: Message to send to the Telegram channel.
+		:arg file_to_send: File that will be sent attached to the message to the Telegram channel.
+		"""
+		url = 'https://api.telegram.org/bot' + str(telegram_bot_token) + '/'
+		data = [("chat_id", telegram_chat_id), ('document', (FORM_FILE, file_to_send))]
+		data.append(("caption", message_to_send))
+		c = Curl()
+		storage = StringIO()
+		c.setopt(c.URL, url + 'sendDocument')
+		c.setopt(c.HTTPPOST, data)
 		c.perform_rs()
 		status_code = c.getinfo(HTTP_CODE)
 		c.close()
